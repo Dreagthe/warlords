3e1e75b39954cfa0c0a95867e824e127957c4363
Zack Gray <zgrizzles@gmail.com>
2023-10-06 20:56:19 -0700

    Update Python bytecode and add new components
    In this commit, we've made the following changes:
    
    - Updated Python bytecode files for existing modules:
      - actions.cpython-311.pyc
      - engine.cpython-311.pyc
      - entity.cpython-311.pyc
      - entity_factories.cpython-311.pyc
      - game_map.cpython-311.pyc
      - input_handlers.cpython-311.pyc
      - procgen.cpython-311.pyc
    
    - Added new Python bytecode file:
      - render_order.cpython-311.pyc
    
    - Modified existing Python source files:
      - actions.py
      - engine.py
      - entity.py
      - game_map.py
      - input_handlers.py
      - main.py
      - procgen.py
      - tile_types.py
    
    - Added new components:
      - components/__init__.py
      - components/ai.py
      - components/base_component.py
      - components/fighter.py
    
    We're enhancing our project's functionality and maintainability by
    updating bytecode and introducing new components.

--- procgen.py
+++ procgen.py
@@ -9,8 +9,10 @@ import entity_factories
 from game_map import GameMap
 import tile_types
 
+
 if TYPE_CHECKING:
-    from entity import Entity
+    from engine import Engine
+
 
 class RectangularRoom:
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -30,7 +32,6 @@ class RectangularRoom:
     def inner(self) -> Tuple[slice, slice]:
         """Return the inner area of this room as a 2D array index."""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
-
 
     def intersects(self, other: RectangularRoom) -> bool:
         """Return True if this room overlaps with another RectangularRoom."""
@@ -57,6 +58,7 @@ def place_entities(
             else:
                 entity_factories.troll.spawn(dungeon, x, y)
 
+
 def tunnel_between(
     start: Tuple[int, int], end: Tuple[int, int]
 ) -> Iterator[Tuple[int, int]]:
@@ -77,7 +79,6 @@ def tunnel_between(
         yield x, y
 
 
-
 def generate_dungeon(
     max_rooms: int,
     room_min_size: int,
@@ -85,10 +86,11 @@ def generate_dungeon(
     map_width: int,
     map_height: int,
     max_monsters_per_room: int,
-    player: Entity,
+    engine: Engine,
 ) -> GameMap:
     """Generate a new dungeon map."""
-    dungeon = GameMap(map_width, map_height, entities=[player])
+    player = engine.player
+    dungeon = GameMap(engine, map_width, map_height, entities=[player])
 
     rooms: List[RectangularRoom] = []
 
@@ -112,7 +114,7 @@ def generate_dungeon(
 
         if len(rooms) == 0:
             # The first room, where the player starts.
-            player.x, player.y = new_room.center
+            player.place(*new_room.center, dungeon)
         else:  # All rooms after the first.
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
--- tile_types.py
+++ tile_types.py
@@ -16,24 +16,25 @@ tile_dt = np.dtype(
     [
         ("walkable", bool),  # True if this tile can be walked over.
         ("transparent", bool),  # True if this tile doesn't block FOV.
-        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV
-        ("light", graphic_dt),  # Graphics for when the tile is in FOV
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt),  # Graphics for when the tile is in FOV.
     ]
 )
+
 
 def new_tile(
     *,  # Enforce the use of keywords, so that parameter order doesn't matter.
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
-    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """Helper function for defining individual tile types """
     return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
-# SHROUD represents unexplored, unseen tiles.
-SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
     walkable=True,
