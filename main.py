7bf034aca1c9d8da024a59c356cdb3132af86163
Zack Gray <zgrizzles@gmail.com>
2023-10-07 09:18:17 -0700

    Title: Implement Enhanced UI Features
    Introduced a centralized color.py file to manage RGB color definitions
    replacing hardcoded values.
    
    Developed a render_bar function in render_functions.py for rendering a
    health bar, aiding in visualizing player HP.
    
    Updated engine.py to utilize the render_bar function,
    improving the HP display aesthetics.
    
    Added a message_log.py to setup the structure for an in-game message
    log, ensuring a more immersive user experience compared to terminal
    message printing​

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import copy
 
 import tcod
 
+
+import color
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
+
 
 
 def main() -> None:
@@ -13,7 +16,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -40,6 +43,10 @@ def main() -> None:
     )
     engine.update_fov()
 
+    engine.message_log.add_message(
+        "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
+    )
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -47,7 +54,7 @@ def main() -> None:
         title="Yet Another Roguelike Tutorial",
         vsync=True,
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
             engine.render(console=root_console, context=context)
 
